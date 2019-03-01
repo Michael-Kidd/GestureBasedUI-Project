9286e7dcece6a2d302e2ad70c0f6c21e2be98129
Michael Kidd <G00236920@gmit.ie>
2019-03-01 10:44:10 +0000

    Added Light Matrix Indications

--- scripts/LightMatrix.py
+++ scripts/LightMatrix.py
@@ -1,46 +1,48 @@
 from sense_hat import SenseHat 
+import threading
+import time
 
-def left():
-    sense.set_pixel(3, 7, (0, 255, 0))
-    sense.set_pixel(4, 7, (0, 255, 0))
-    sense.set_pixel(3, 6, (0, 255, 0))
-    sense.set_pixel(4, 6, (0, 255, 0))
-    sense.set_pixel(2, 6, (0, 255, 0))
-    sense.set_pixel(5, 6, (0, 255, 0))
+def left(r, g, b):
+    sense.set_pixel(3, 7, (r, g, b))
+    sense.set_pixel(4, 7, (r, g, b))
+    sense.set_pixel(3, 6, (r, g, b))
+    sense.set_pixel(4, 6, (r, g, b))
+    sense.set_pixel(2, 6, (r, g, b))
+    sense.set_pixel(5, 6, (r, g, b))
 
-def right():
-    sense.set_pixel(3, 0, (0, 255, 0))
-    sense.set_pixel(4, 0, (0, 255, 0))
-    sense.set_pixel(3, 1, (0, 255, 0))
-    sense.set_pixel(4, 1, (0, 255, 0))
-    sense.set_pixel(2, 1, (0, 255, 0))
-    sense.set_pixel(5, 1, (0, 255, 0))
+def right(r, g, b):
+    sense.set_pixel(3, 0, (r, g, b))
+    sense.set_pixel(4, 0, (r, g, b))
+    sense.set_pixel(3, 1, (r, g, b))
+    sense.set_pixel(4, 1, (r, g, b))
+    sense.set_pixel(2, 1, (r, g, b))
+    sense.set_pixel(5, 1, (r, g, b))
 
-def forward():
-    sense.set_pixel(0, 3, (0, 0, 255))
-    sense.set_pixel(0, 4, (0, 0, 255))
-    sense.set_pixel(1, 3, (0, 0, 255))
-    sense.set_pixel(1, 4, (0, 0, 255))
-    sense.set_pixel(1, 2, (0, 0, 255))
-    sense.set_pixel(1, 5, (0, 0, 255))
+def forward(r, g, b):
+    sense.set_pixel(0, 3, (r, g, b))
+    sense.set_pixel(0, 4, (r, g, b))
+    sense.set_pixel(1, 3, (r, g, b))
+    sense.set_pixel(1, 4, (r, g, b))
+    sense.set_pixel(1, 2, (r, g, b))
+    sense.set_pixel(1, 5, (r, g, b))
 
-def reverse():
-    sense.set_pixel(7, 3, (255, 0, 0))
-    sense.set_pixel(7, 4, (255, 0, 0))
-    sense.set_pixel(6, 3, (255, 0, 0))
-    sense.set_pixel(6, 4, (255, 0, 0))
-    sense.set_pixel(6, 2, (255, 0, 0))
-    sense.set_pixel(6, 5, (255, 0, 0))
+def reverse(r, g, b):
+    sense.set_pixel(7, 3, (r, g, b))
+    sense.set_pixel(7, 4, (r, g, b))
+    sense.set_pixel(6, 3, (r, g, b))
+    sense.set_pixel(6, 4, (r, g, b))
+    sense.set_pixel(6, 2, (r, g, b))
+    sense.set_pixel(6, 5, (r, g, b))
 
 def clear():
     # Display these colours on the LED matrix
     sense.clear()
 
-
-
 sense = SenseHat()
+
 clear()
-left()
-right()
-forward()
-reverse()
+
+threading.Thread(target=left(0, 255, 0)).start()
+threading.Thread(target=right(0, 255 ,0)).start()
+threading.Thread(target=forward(0, 0, 255)).start()
+threading.Thread(target=reverse(255, 0, 0)).start()
