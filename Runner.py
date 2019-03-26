86f04eb8080c4a16b0f8e117a7aaba02fa5d21ae
Michael Kidd <g00306474@gmit.ie>
2019-03-26 14:26:37 +0000

    added comments for testing

--- Runner.py
+++ Runner.py
@@ -1,31 +1,36 @@
 from __future__ import print_function
+
 import enum
 import re
 import struct
 import sys
 import threading
 import time
+
 import serial
 from serial.tools.list_ports import comports
-from myo_raw import BT, MyoRaw
 
+from myo_raw import BT, MyoRaw
 from PyoConnect import *
+
 
 def onPoseEdge(p):
 
 	if p == p.REST:
 		myo.rotSetCenter()
-		print("MYo Connected")
+		print("MYO REST")
 	if p == p.FIST:
 		if myo.getBox() == 7:
 			myo.vibrate(3)
-			print("MYo Left?")
+			print("MYO Left?")
 		elif myo.getBox() == 3:
 			myo.vibrate(4)
-			print("MYo Right?")
+			print("MYO Right?")
 		else:
+			print("else")
 			myo.vibrate(2)
 	if p == p.WAVE_OUT:
+		print("WAVE_OUT")
 		myo.vibrate(1)
 
 
@@ -33,14 +38,14 @@ if __name__ == '__main__':
 
 	# Connect the myo armband
 	myo = Myo(sys.argv[1] if len(sys.argv) >= 2 else None) 
-	print("MYo Connect")
+	print("MYo Connected")
 	myo.connect()
-
+	print("MYo Connect 2")
 	# arm handler, left and right arms
 	# also handles the direction, either facing elbow or wrist
 	# myo.add_arm_handler(lambda arm, xdir: print('arm', arm, 'xdir', xdir))
 	myo.add_pose_handler(lambda p: onPoseEdge(p))
-
+	print("MYo Connect 3")
 	while True:
 		# keep the Myo active
 		myo.run()
