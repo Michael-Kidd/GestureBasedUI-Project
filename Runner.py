6e7ce5c966e6548a9ddd86e85382bf5348630b5e
G00306474 <G00306474@gmit.ie>
2019-03-29 13:38:18 +0000

    runner changed to if from switch

--- Runner.py
+++ Runner.py
@@ -10,6 +10,8 @@ import time
 import serial
 from serial.tools.list_ports import comports
 
+import robohat
+
 from myo_raw import BT, MyoRaw
 from PyoConnect import *
 
@@ -17,35 +19,46 @@ from PyoConnect import *
 def onPoseEdge(p):
 
 	if p == p.REST:
+		print("Rest")
+		robohat.stop()
 		myo.rotSetCenter()
-		print("MYO REST")
+		#robohat.cleanup()
 	if p == p.FIST:
+		print("Fist")
 		if myo.getBox() == 7:
-			myo.vibrate(3)
-			print("MYO Left?")
+			print("Turn Left")
+			robohat.turnForward(0, speed)
 		elif myo.getBox() == 3:
-			myo.vibrate(4)
-			print("MYO Right?")
+			print("Turn Right")
+			robohat.turnForward(speed, 0)
 		else:
-			print("else")
-			myo.vibrate(2)
+			print("forward")
+			robohat.forward(speed)
 	if p == p.WAVE_OUT:
-		print("WAVE_OUT")
-		myo.vibrate(1)
-
+		print("Spin Right")
+		robohat.spinRight(speed)
+		myo.rotSetCenter()
+	if p == p.WAVE_IN:
+		print("Spin Left")
+		robohat.spinLeft(speed)
+		myo.rotSetCenter()
+	if p == p.FINGERS_SPREAD:
+		print("Reverse")
+		robohat.reverse(speed)
+		myo.rotSetCenter()
 
 if __name__ == '__main__':
 
 	# Connect the myo armband
-	myo = Myo(sys.argv[1] if len(sys.argv) >= 2 else None) 
-	print("MYo Connected")
+	myo = Myo(sys.argv[1] if len(sys.argv) >= 2 else None)
+
+	speed = 40
 	myo.connect()
-	print("MYo Connect 2")
+	robohat.init()
 	# arm handler, left and right arms
 	# also handles the direction, either facing elbow or wrist
 	# myo.add_arm_handler(lambda arm, xdir: print('arm', arm, 'xdir', xdir))
 	myo.add_pose_handler(lambda p: onPoseEdge(p))
-	print("MYo Connect 3")
 	while True:
 		# keep the Myo active
 		myo.run()
